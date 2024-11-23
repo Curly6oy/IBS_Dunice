921da5f577ac7f88722ab1a0f27ce4fb0da5fc42
iamze9 <114343778+Curly6oy@users.noreply.github.com>
2024-11-23 22:10:55 +0300

    Delete backend/backend directory

--- backend/backend/common/mapUtil.js
+++
@@ -1,7 +0,0 @@
-
-const array2map = (array, keyField) => array.reduce((map, element) => {
-        map[element[keyField]] = element
-        return map
-    }, {})
-
-module.exports = {array2map}    
